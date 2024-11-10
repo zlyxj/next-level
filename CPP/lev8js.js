1cb5104cae36d62e50ba92059788bbb539ebc1db
zlyxj <xiaomei8229@sina.com>
2024-11-10 10:54:43 +0800

    Create lev8js.js

---
+++ CPP/lev8js.js
@@ -0,0 +1,9 @@
+function checkans()
+{
+  var codeans=document.getElementById("codeans").value
+  if(codeans === "int main()"){
+    window.location.href=("/next-level/CPP/apple.html");
+  }else{
+    alert("答案错误")
+  }
+}
