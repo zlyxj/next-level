27f8885ea6c2e94fd43c83da1896a301b9ae1a1d
zlyxj <xiaomei8229@sina.com>
2024-11-10 10:23:29 +0800

    Create lev6js.js

---
+++ myfolder/lev6js.js
@@ -0,0 +1,9 @@
+function checkans()
+{
+  var mathans=document.getElementById("mathans").value
+  if(mathans === "-2-sqrt(3)"){
+    window.location.href=("/myfolder/seventh.html");
+  }else{
+    alert("答案错误（如果答案是正确的，交换一下位置试试？）")
+  }
+}
